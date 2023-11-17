6eafb8e573f99d3233f04483fa9db9d464be5397
Ahmed Adebisi <adebisi.ahmed10@gmail.com>
2023-11-17 12:07:05 +0000

    feat: add Ai prompt

--- main.go
+++ main.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/AdePhil/plotter/cmd"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+			log.Fatal(err)
+	}
+	fmt.Print(dir)
+	err = godotenv.Load(filepath.Join("./", ".env"))
+	
   if err != nil {
-    log.Fatal("Error loading .env file")
+    log.Fatal("Error loading .env file here", err)
   }
 
 	cmd.RootCmd.Execute()
